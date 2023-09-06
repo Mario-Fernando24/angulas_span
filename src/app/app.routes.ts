d0e3b461fd6a5fc2d373ad68f961fdd653ea9ab6
MARIO MUÑOZ <mariocamila24@gmail.com>
2023-09-06 15:22:58 -0500

    agregar routes home, heroes and about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import {RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'
+import { AboutComponent } from './components/about/about.component'
+import { HeroesComponent } from './components/heroes/heroes.component'
+
  
 export const APP_ROUTERS: Routes = [
     { path: 'home', component: HomeComponent },
-    // { path: 'otra-ruta', component: OtraComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
   ];
